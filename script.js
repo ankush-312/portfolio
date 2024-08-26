44621ed01d1ad7083060b7198e8061246fc87cc3
ankush <aptech631@gmail.com>
2024-08-27 01:14:50 +0530

    Initial commit

--- script.js
+++ script.js
@@ -25,3 +25,28 @@ const observer = new IntersectionObserver(entries => {
 skillCards.forEach(card => {
     observer.observe(card);
 });
+
+
+// navbar design
+
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", mobileMenu);
+
+function mobileMenu() {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+}
+
+
+// when we click on hamburger icon its close 
+
+const navLink = document.querySelectorAll(".nav-link");
+
+navLink.forEach(n => n.addEventListener("click", closeMenu));
+
+function closeMenu() {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+}
